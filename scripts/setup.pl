553dd7326b1da1bb4453927b07d3a586326fc067
takadonet <philipmabon@gmail.com>
2014-08-20 09:27:59 -0500

    removed all mention and instruction about gview.

--- scripts/setup.pl
+++ scripts/setup.pl
@@ -65,15 +65,6 @@ print STDERR "Checking for Software dependencies...\n";
 my $paths = $config->{'path'};
 check_software($paths);
 
-my $gview_style_path = "$script_dir/../etc/original.gss";
-if (not -e $gview_style_path)
-{
-	die "error; could not find gview style in $gview_style_path";
-}
-else
-{
-	$config->{'gview_style'} = $gview_style_path;
-}
 
 if (not $force and -e $out_config_file)
 {
@@ -122,7 +113,6 @@ sub check_software
 	my ($paths) = @_;
 	
 	# remote special paths where key name does not correspond to binary name
-	delete $paths->{'gview'};
 	delete $paths->{'mummer2vcf'};
 	delete $paths->{'vcftools-lib'};
 
@@ -143,19 +133,6 @@ sub check_software
 		}
 	}
 
-	print STDERR "Checking for GView ...";
-	my $binary_name = "gview";
-	my $binary_path = `which gview.jar`;
-	chomp $binary_path;
-	if (not -e $binary_path)
-	{
-		die "error: $binary_name could not be found on PATH";
-	}
-	else
-	{
-		print STDERR "OK\n";
-		$paths->{$binary_name} = $binary_path;
-	}
 
 	print STDERR "Checking for mummer2Vcf ...";
 	my $binary_name = "mummer2Vcf.pl";
